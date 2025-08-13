9b654ee54203e339c1d2c15a2bd51c8befdf4943
hr0dv1tn1r <rafaelludtech@gmail.com>
2025-08-13 10:00:16 -0300

    Split Screen keybind like tmux

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -19,6 +19,12 @@ vim.keymap.set('n', "<leader>ce", function()
     require("CopilotChat").ask_followup("Explique o código selecionado", { selection = require("CopilotChat.select").get_selection() })
 end, { desc = "Copilot: Explicar código selecionado"})
 
+-- Atalhos para dividir a tela (split) no estilo do Tmux
+vim.keymap.set("n", "<leader>\"", "<cmd>split<CR>", { desc = "Split Horizontal (Tmux style)" })
+vim.keymap.set("n", "<C-w>\"", "<cmd>split<CR>", { desc = "Split Horizontal (C-w)" })
+vim.keymap.set("n", "<leader>%", "<cmd>vsplit<CR>", { desc = "Split Vertical (Tmux style)" })
+vim.keymap.set("n", "<C-w>%", "<cmd>vsplit<CR>", { desc = "Split Vertical (C-w)" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 --map("n", ";", ":", { desc = "CMD enter command mode" })
 --map("i", "jk", "<ESC>")
