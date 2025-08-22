2ca95760614cd5261983b1549db332527e37f2bc
hr0dv1tn1r <rafaelludtech@gmail.com>
2025-08-22 08:49:25 -0300

    nvim keybinds

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -15,9 +15,16 @@ vim.keymap.set('n', '<leader>h', '<cmd>nohlsearch<CR>', { silent = true })
 
 -- Atalhos de teclado para o Copilot Chat
 vim.keymap.set('n', "<leader>cc", "<cmd>CopilotChat<CR>", { desc = "Abrir Chat do Copilot" })
-vim.keymap.set('n', "<leader>ce", function()
-    require("CopilotChat").ask_followup("Explique o código selecionado", { selection = require("CopilotChat.select").get_selection() })
-end, { desc = "Copilot: Explicar código selecionado"})
+vim.keymap.set('v', "<leader>ce", "<cmd>CopilotChatExplain<CR>", { desc = "Explique o Código selecionado" })
+vim.keymap.set('v', "<leader>cr", "<cmd>CopilotChatReview<CR>", { desc = "Review Code" })
+vim.keymap.set('v', "<leader>cf", "<cmd>CopilotChatFix<CR>", { desc = "Fix Code Issues" })
+vim.keymap.set('v', "<leader>co", "<cmd>CopilotChatOptimize<CR>", { desc = "Optimize Issues" })
+vim.keymap.set('v', "<leader>cd", "<cmd>CopilotChatDocs<CR>", { desc = "Generate Docs" })
+vim.keymap.set('v', "<leader>ct", "<cmd>CopilotChatTests<CR>", { desc = "Generate Tests" })
+vim.keymap.set('v', "<leader>c", "<cmd>CopilotChatFix<CR>", { desc = "Fix Code Issues" })
+-- vim.keymap.set('n', "<leader>ce", function()
+--     require("CopilotChat").ask_followup("Explique o código selecionado", { selection = require("CopilotChat.select").get_selection() })
+-- end, { desc = "Copilot: Explicar código selecionado"})
 
 -- Atalhos para dividir a tela (split) no estilo do Tmux
 vim.keymap.set("n", "<leader>\"", "<cmd>split<CR>", { desc = "Split Horizontal (Tmux style)" })
